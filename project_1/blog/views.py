97c88f3b0688e909f479a344de7d4ea9352b9737
dmurawski018 <dmurawski018@gmail.com>
2025-05-05 19:26:53 +0200

    add form to send email with recommendation post

--- project_1/blog/views.py
+++ project_1/blog/views.py
@@ -1,7 +1,32 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
+from .forms import EmailPostForm
 from .models import Post
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+        },
+    )
 
 
 def post_list(request):
