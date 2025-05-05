54514ddb29096e490ed6c155882324b6238d7baf
dmurawski018 <dmurawski018@gmail.com>
2025-05-05 16:02:14 +0200

    add detail view

--- project_1/blog/views.py
+++ project_1/blog/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.published.all()
     context = {"posts": posts}
 
     return render(
         request,
         "blog/post/list.hmtl",
+        context,
+    )
+
+
+def post_detail(request, id):
+    posts = get_object_or_404(
+        Post,
+        id,
+        status=Post.Status.PUBLISHED,
+    )
+    context = {"posts": posts}
+
+    return render(
+        request,
+        "blog/post/detail.hmtl",
         context,
     )
