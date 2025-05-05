a448b6de5fefe948bab8456716d156b97090b349
dmurawski018 <dmurawski018@gmail.com>
2025-05-05 19:15:51 +0200

    handling pagination error page not an integer

--- project_1/blog/views.py
+++ project_1/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import EmptyPage, Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
@@ -11,6 +11,9 @@ def post_list(request):
     page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # return first page
+        posts = paginator.page(1)
     except EmptyPage:
         # return last page
         posts = paginator.page(paginator.num_pages)
