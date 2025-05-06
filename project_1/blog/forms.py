7312c5406142f9ea2ead36c61d32e87663662858
dmurawski018 <dmurawski018@gmail.com>
2025-05-06 16:04:22 +0200

    end feature comment system

--- project_1/blog/forms.py
+++ project_1/blog/forms.py
@@ -1,9 +1,11 @@
 from crispy_bootstrap5.bootstrap5 import FloatingField
-from crispy_forms.bootstrap import Field, Tab, TabHolder
+from crispy_forms.bootstrap import Field
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import HTML, Div, Field, Fieldset, Layout, Submit
 from django import forms
 from django.urls import reverse
+
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -43,8 +45,46 @@ class EmailPostForm(forms.Form):
             HTML("<hr>"),
             Fieldset(
                 "Wiadomość",  # Trzecia sekcja
-                Field("comments", label="", rows="5", cols="0"),
+                Field("comments", rows="5", cols="0"),
                 cols="12",
             ),
             Submit("submit", "Send e-mail", css_class="btn btn-primary col-12 fw-bold"),
+        )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("name", "email", "body")
+
+    def __init__(self, *args, **kwargs):
+        post_id = kwargs.pop("post_id", None)
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+
+        if post_id:
+            self.helper.form_action = reverse(
+                "blog:post_comment", kwargs={"post_id": post_id}
+            )
+
+        self.helper.form_class = "form-horizontal mt-5"
+        self.helper.label_class = "col-sm-3 col-form-label"
+        self.helper.field_class = "p-2"
+
+        self.helper.layout = Layout(
+            Fieldset(
+                "Autor komentarza",
+                FloatingField("name", placeholder="Your name"),
+                FloatingField("email", placeholder="Your email"),
+            ),
+            HTML("<hr>"),
+            Fieldset(
+                "Treść komentarza",
+                FloatingField("body", placeholder="Napisz coś..."),
+            ),
+            Submit(
+                "submit", "Dodaj komentarz", css_class="btn btn-primary col-12 fw-bold"
+            ),
         )
