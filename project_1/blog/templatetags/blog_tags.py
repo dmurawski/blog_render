7464675fe976d937bb1aa2658892464eadd265a1
dmurawski018 <dmurawski018@gmail.com>
2025-05-07 23:49:13 +0200

    template tag that returns a queryset

--- project_1/blog/templatetags/blog_tags.py
+++ project_1/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from ..models import Post
 
@@ -14,3 +15,12 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(
+        total_comments=Count("comments")
+    ).order_by("-total_comments")[:count]
+
+    return most_commented_posts
